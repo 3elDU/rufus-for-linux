ad93ca65da7873a8bc23be5371a77096a06e34cb
tfastgame <tfastgame@gmail.com>
2022-03-21 20:04:29 +0200

    fix umount device

--- write.py
+++ write.py
@@ -4,12 +4,12 @@ import sys
 
 def formate_usb(device:str, file_system:str, name:str):
     print("unmountig device")
-    os.system("umount -r "+device)
+    os.system("umount -rf "+device)
     print("Wipe device")
     os.system("mkfs."+file_system+" -I -n "+name+" "+device)
 
 def write_iso(iso_file:str, device_to_write:str):
-    os.system("umount "+iso_file)
+    os.system("umount -f "+iso_file)
     formate_usb(device=device_to_write, file_system="vfat", name="FLASH")
     print("mounting iso...")
     os.system("mkdir /mnt/iso")
@@ -20,8 +20,8 @@ def write_iso(iso_file:str, device_to_write:str):
     print("writing...")
     os.system("cp -rLv /mnt/iso/* /mnt/flash")
     print("DONE!!!")
-    os.system("umount -r "+device_to_write)
-    os.system("umount "+iso_file)
+    os.system("umount -rf "+device_to_write)
+    os.system("umount -f "+iso_file)
 
 
 write_iso(iso_file=sys.argv[1], device_to_write=sys.argv[2])
